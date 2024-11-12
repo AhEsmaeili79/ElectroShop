f21313403ccb1d617b64217cd71b15e35958770c
Amirhossein Esmaeili <ah.esmaeili.79@gmail.com>
2024-11-13 01:53:51 +0330

    UpdateSerializer

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -1,9 +1,13 @@
 # product/serializers.py
+
 from rest_framework import serializers
 from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    # Access seller's name directly
+    seller = serializers.CharField(source="seller.username", read_only=True)
+
     class Meta:
         model = Product
-        fields = ["id", "name", "quantity", "price", "main_photo", "model"]
+        fields = ["id", "name", "quantity", "price", "main_photo", "model", "seller"]
