35e0258b058be70a6c7aedf5b7eaf87947dc5cb7
Amirhossein Esmaeili <ah.esmaeili.79@gmail.com>
2024-11-15 23:58:41 +0330

    add_category_Product

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,7 +1,7 @@
 # product/models.py
 from django.db import models
 from django.conf import settings
-
+from category.models import Category
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -17,6 +17,7 @@ class Product(models.Model):
     photo3 = models.ImageField(upload_to="products/%Y/%m/%d/", blank=True)
     photo4 = models.ImageField(upload_to="products/%Y/%m/%d/", blank=True)
     seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.PROTECT)
 
     def __str__(self):
         return (
