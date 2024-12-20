5c733a3b4ffa622420323f9bddbc651d47c7e6d0
Amirhossein Esmaeili <ah.esmaeili.79@gmail.com>
2024-12-20 03:36:51 +0330

    feat: initialize project react structure and add base files

--- frontend/src/pages/ProductDetailPage/ProductDetailTop/Components/WishlistButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const WishlistButton = ({ isFavorited, handleAddToWishlist }) => (
-  <a className="btn-product btn-wishlist" title="Wishlist" onClick={handleAddToWishlist}>
-    <span>{isFavorited ? "Remove from Wishlist" : "Add to Wishlist"}</span>
-  </a>
-);
-
-export default WishlistButton;
