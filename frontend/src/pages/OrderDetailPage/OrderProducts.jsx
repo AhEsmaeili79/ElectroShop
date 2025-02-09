2e1509cc3be5e6d380540bd96687586e7e308316
Amirhossein Esmaeili <ah.esmaeili.79@gmail.com>
2025-02-09 04:41:11 +0330

    style : style order detail , payment status

--- frontend/src/pages/OrderDetailPage/OrderProducts.jsx
+++ frontend/src/pages/OrderDetailPage/OrderProducts.jsx
@@ -17,6 +17,17 @@ const OrderProducts = ({ order }) => {
     return statusMapping[status] || status;
   };
 
+  const getStatusBadgeClass = (status) => {
+    const badgeClasses = {
+      canceled: 'text-danger',
+      successful: 'text-success',
+      pending: 'text-warning',
+      delivered: 'text-primary',
+      returned: 'text-info',
+    };
+    return badgeClasses[status] || 'text-secondary';
+  };
+
   const calculateTotalPrice = (items, shipmentPrice) => {
     const itemsTotal = items.reduce((total, item) => {
       const price = parseFloat(item.product_price) || 0;
@@ -32,86 +43,105 @@ const OrderProducts = ({ order }) => {
 
   return (
     <>
-      <div className="col-lg-9">
-        <div className="products mb-3">
-          {order.items.map((item) => (
-            <div className="product product-list" key={item.id}>
-              <div className="row">
-                <div className="col-6 col-lg-3">
-                  <figure className="product-media center">
-                    <Link to={`/product/${item.product}`}>
-                      <img
-                        src={item.product_image}
-                        alt={item.product_name}
-                        className="product-image"
-                      />
-                    </Link>
-                  </figure>
-                </div>
-                <div className="col-lg-6">
-                  <div className="product-body product-action-inner">
-                    <div className="order-summary">
-                      <div className="d-flex justify-content-between">
-                        <strong>نام محصول:</strong>
-                        <Link to={`/product/${item.product}`}>
-                          <h5>{item.product_name}</h5>
-                        </Link>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-lg-9 col-md-12">
+            <div className="mb-4">
+              <h3 className="text-center text-success mb-4">جزئیات سفارش</h3>
+              <div className="card shadow-lg border-0 rounded-3">
+                <div className="card-body p-4">
+                  {order.items.map((item) => (
+                    <div className="d-flex mb-4 border-bottom pb-4" key={item.id}>
+                      <div className="col-6 col-lg-3">
+                        <figure className="product-media">
+                          <Link to={`/product/${item.product}`}>
+                            <img
+                              src={item.product_image}
+                              alt={item.product_name}
+                              className="img-fluid rounded shadow-sm"
+                            />
+                          </Link>
+                        </figure>
                       </div>
-                      <div className="d-flex justify-content-between">
-                        <strong>قیمت:</strong>{" "}
-                        <span>{toPersianNumbers(item.product_price)} تومان</span>
-                      </div>
-                      <div className="d-flex justify-content-between">
-                        <strong>فروشنده:</strong> <span>{item.product_seller}</span>
-                      </div>
-                      <div className="d-flex justify-content-between">
-                        <strong>تعداد:</strong> <span>{toPersianNumbers(item.quantity)}</span>
+                      <div className="col-lg-6">
+                        <div className="product-body">
+                          <div className="order-summary">
+                            <div className="d-flex justify-content-between">
+                              <strong>نام محصول:</strong>
+                              <Link to={`/product/${item.product}`}>
+                                <h5 className="text-primary">{item.product_name}</h5>
+                              </Link>
+                            </div>
+                            <div className="d-flex justify-content-between">
+                              <strong>قیمت:</strong>
+                              <span>{toPersianNumbers(item.product_price)} تومان</span>
+                            </div>
+                            <div className="d-flex justify-content-between">
+                              <strong>فروشنده:</strong> <span>{item.product_seller}</span>
+                            </div>
+                            <div className="d-flex justify-content-between">
+                              <strong>تعداد:</strong> <span>{toPersianNumbers(item.quantity)}</span>
+                            </div>
+                          </div>
+                          <div className="d-flex justify-content-between mt-3">
+                            <strong>رنگ:</strong>
+                            <p
+                              style={{
+                                display: "inline-block",
+                                width: "24px",
+                                height: "24px",
+                                backgroundColor: item.color.color_hex,
+                                border: "2px solid #dbdbdb",
+                                borderRadius: "50%",
+                                marginLeft: "5px",
+                              }}
+                            ></p>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                    <div className="d-flex justify-content-between">
-                      <strong>رنگ:</strong>
-                      <p
-                        style={{
-                          display: "inline-block",
-                          width: "24px",
-                          height: "24px",
-                          backgroundColor: item.color.color_hex,
-                          border: "2px solid #dbdbdb",
-                          borderRadius: "50%",
-                          marginLeft: "5px",
-                        }}
-                      ></p>
-                    </div>
-                  </div>
+                  ))}
                 </div>
               </div>
             </div>
-          ))}
-        </div>
-        <div className="order-summary">
-          <div className="d-flex justify-content-between">
-            <strong>کد سفارش:</strong> <span>{toPersianNumbers(order.order_code)}</span>
-          </div>
-          <div className="d-flex justify-content-between">
-            <strong>تاریخ:</strong>{" "}
-            <span>{toPersianNumbers(convertToPersianDate(order.payment.created_at))}</span>
-          </div>
-          <div className="d-flex justify-content-between">
-            <strong>وضعیت سفارش:</strong> <span>{getStatusInPersian(order.status)}</span>
-          </div>
-          <div className="d-flex justify-content-between">
-            <strong>کد پیگیری:</strong> <span>{toPersianNumbers(order.payment.transaction_ref_id)}</span>
-          </div>
-          <div className="d-flex justify-content-between">
-            <strong>نوع پرداخت:</strong> <span>{order.payment_type === 'cash' ? 'نقد' : 'خرید اعتباری'}</span>
-          </div>
-          <div className="d-flex justify-content-between">
-            <strong>هزینه ارسال:</strong>{" "}
-            <span>{toPersianNumbers(order.shipment_price_amount)} تومان</span>
-          </div>
-          <div className="d-flex justify-content-between">
-            <strong>مجموع قیمت:</strong>{" "}
-            <span>{toPersianNumbers(calculateTotalPrice(order.items, order.shipment_price))} تومان</span>
+
+            <div className="card shadow-lg mt-4 border-0 rounded-3">
+              <div className="card-body p-4">
+                <h5 className="mb-4 text-dark">اطلاعات سفارش</h5>
+                <div className="d-flex justify-content-between mb-3">
+                  <strong>کد سفارش:</strong> <span>{toPersianNumbers(order.order_code)}</span>
+                </div>
+                <div className="d-flex justify-content-between mb-3">
+                  <strong>تاریخ:</strong>{" "}
+                  <span>{toPersianNumbers(convertToPersianDate(order.payment.created_at))}</span>
+                </div>
+                <div className="d-flex justify-content-between mb-3">
+                  <strong>وضعیت سفارش:</strong>
+                  <span
+                    className={`${getStatusBadgeClass(order.status)}`}
+                    style={{ fontSize: "1.5rem" }}
+                  >
+                    {getStatusInPersian(order.status)}
+                  </span>
+                </div>
+                <div className="d-flex justify-content-between mb-3">
+                  <strong>کد پیگیری:</strong> 
+                  <span>{order.payment.transaction_ref_id ? toPersianNumbers(order.payment.transaction_ref_id) : 'فاقد کد پیگیری'}</span>
+                </div>
+
+                <div className="d-flex justify-content-between mb-3">
+                  <strong>نوع پرداخت:</strong> <span>{order.payment_type === 'cash' ? 'نقد' : 'خرید اعتباری'}</span>
+                </div>
+                <div className="d-flex justify-content-between mb-3">
+                  <strong>هزینه ارسال:</strong>{" "}
+                  <span>{toPersianNumbers(order.shipment_price_amount)} تومان</span>
+                </div>
+                <div className="d-flex justify-content-between mb-3">
+                  <strong>مجموع قیمت:</strong>{" "}
+                  <span>{toPersianNumbers(calculateTotalPrice(order.items, order.shipment_price))} تومان</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
